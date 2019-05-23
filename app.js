867649de9000dabe1c72d27bc4b25e071b893600
sussica <sussicaya@gmail.com>
2019-05-22 22:57:53 -0700

    Added data base(mongo db) to current website

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash = require('lodash');
+const mongoose = require('mongoose');
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent = "Hey"
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
@@ -18,15 +19,42 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static("public"));
 
-var posts = [];
+
+
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+  useNewUrlParser: true
+})
+
+
+const postSchema = {
+  inputTitle: String,
+  inputPost: String
+}
+
+const Post = mongoose.model(
+  "Post",
+  postSchema
+)
+
+const startPost = new Post({
+  inputTitle: "Welcome",
+  inputPost: 'Welcome to your blog! You can add "/compose" to current url to add new post! Enjoy your blog time!'
+})
 
 
 app.route(['/', '/contact', '/about', '/compose'])
   .get(function(req, res) {
     if (req.url == '/') {
-      res.render('home', {
-        DisplayContent: homeStartingContent,
-        newPosts:posts
+      Post.find({}, function(err, results) {
+        if (results.length === 0) {
+          startPost.save();
+          res.redirect('/');
+        } else {
+          res.render('home', {
+            DisplayContent: homeStartingContent,
+            newPosts: results
+          });
+        }
       });
     } else if (req.url == '/contact') {
       res.render('contact', {
@@ -42,22 +70,34 @@ app.route(['/', '/contact', '/about', '/compose'])
   });
 
 app.post('/compose', function(req, res) {
-  var post = {
-    inputTitle: req.body.titleInput,
-    inputPost: req.body.postInput
-  }
-  posts.push(post);
-  res.redirect('/');
+  const inputTitle = req.body.titleInput;
+  const inputPost = req.body.postInput;
+
+  const newPost = new Post({
+    inputTitle: inputTitle,
+    inputPost: inputPost
+  })
+
+  newPost.save(function(err) {
+    if (!err) {
+      res.redirect('/');
+    }
+  });
+
 })
 
-app.get('/posts/:postsID', function (req, res) {
-  posts.forEach(function(post){
-    if(lodash.lowerCase(req.params.postsID) == lodash.lowerCase(post.inputTitle))
-    res.render('post', {
-      title: post.inputTitle,
-      DisplayContent: post.inputPost
-    })
-  });
+app.get('/posts/:postID', function(req, res) {
+  Post.findOne({
+    _id: req.params.postID
+  }, function(err, results) {
+    if (!err) {
+
+        res.render('post', {
+          title: results.inputTitle,
+          DisplayContent: results.inputPost
+        });    
+    }
+  })
 })
 
 app.listen(3000, function() {
